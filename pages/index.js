964bddd25f83364ca5be975b715e0f17bb264b3a
yeah nah <43940641+hwhi634@users.noreply.github.com>
2021-06-01 10:13:06 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Home() {
   const handleSubmit = (e) => {
     e.preventDefault()
-    fetch('api/file', {
+    fetch('api/data', {
 			method: 'POST',
 			body: JSON.stringify({
 				turl: "https://e-cdns-proxy-a.dzcdn.net/mobile/1/261e5e207714174212fc6dcfb1f355d910717451e447191827f82235e60c672abc3df997fbe8e18fb43c3901ac7aea16705405e69f9a5d823d8e059dc826d6dd2fd0c2384c93e33f267af1cffba07db8"
