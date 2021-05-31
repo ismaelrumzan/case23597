87f35f56e958519b3a6a276abf0fe46e9b09de9c
yeah nah <43940641+hwhi634@users.noreply.github.com>
2021-06-01 10:05:39 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       'content-type': 'application/json',
       },
       body: JSON.stringify({
-      arl: "non",
+      arl: "c610a21568c03931baf2d3e09bac75dc40a7bdd8adade7f881a25c0c15ec714acf4dae21d53387d6b5ec0b72e85cc15af63f586b065939a13407e0e243208b5a4fc83924d5c633f43fc76a68e255f4bff2e637d3b7ee1a42cdb9c2d3495f7892",
       track: "https://www.deezer.com/en/track/75859862"
       }),
     })
